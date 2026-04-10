67d0ecb5126c02d30d137b64315bc1b522c2a586
Adit <aditkirtani@gmail.com>
2026-04-10 11:42:17 +0200

    TAY, TXA, TYA tests

--- tests/tTransfer.cpp
+++ tests/tTransfer.cpp
@@ -2,14 +2,14 @@
 #include <catch2/catch_test_macros.hpp>
 #include <cstdio>
 
-TEST_CASE("TXA Tests", "[Transfer][TXA][IMP]")
+TEST_CASE("TAX tests", "[Transfer][TXA][IMP]")
 {
     Emulator emu;
     emu.load_bytes_at_address(0xFFFD, std::vector<u8> {0x2, 0x0}); // Tells the CPU where to go after reset. PC breaks without this
     emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x8F, 0xAA});
     emu.reset(emu.RST_CPU);
     
-    SECTION("Verify accumulator value")
+    SECTION("Verify register X value")
     {
         emu.execute(4);
         auto cpu = emu.get_CPU_obj();
@@ -46,6 +46,162 @@ TEST_CASE("TXA Tests", "[Transfer][TXA][IMP]")
     SECTION("Verify Z flag cleared")
     {
         emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x1, 0xA0, 0x0, 0xAA});
+        emu.execute(6);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+    }
+
+}
+
+TEST_CASE("TAY tests", "[Transfer][TYA][IMP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFD, std::vector<u8> {0x2, 0x0}); // Tells the CPU where to go after reset. PC breaks without this
+    emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x8F, 0xA8});
+    emu.reset(emu.RST_CPU);
+    
+    SECTION("Verify register Y value")
+    {
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(state.Y == 0x8F);
+    }
+
+    SECTION("Verify N flag set")
+    {
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+
+    SECTION("Verify N flag cleared")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x1, 0xA0, 0x8F, 0xA8});
+        emu.execute(6);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("Verify Z flag set")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x0, 0xA8});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+    }
+
+    SECTION("Verify Z flag cleared")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x1, 0xA0, 0x0, 0xA8});
+        emu.execute(6);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+    }
+
+}
+
+TEST_CASE("TXA tests", "[Transfer][TXA][IMP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFD, std::vector<u8> {0x2, 0x0}); // Tells the CPU where to go after reset. PC breaks without this
+    emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x8F, 0x8A});
+    emu.reset(emu.RST_CPU);
+    
+    SECTION("Verify accumulator value")
+    {
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(state.A == 0x8F);
+    }
+
+    SECTION("Verify N flag set")
+    {
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+
+    SECTION("Verify N flag cleared")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x1, 0xA0, 0x8F, 0x8A});
+        emu.execute(6);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("Verify Z flag set")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x0, 0x8A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+    }
+
+    SECTION("Verify Z flag cleared")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x1, 0xA0, 0x0, 0x8A});
+        emu.execute(6);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+    }
+
+}
+
+TEST_CASE("TYA tests", "[Transfer][TYA][IMP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFD, std::vector<u8> {0x2, 0x0}); // Tells the CPU where to go after reset. PC breaks without this
+    emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x8F, 0x98});
+    emu.reset(emu.RST_CPU);
+    
+    SECTION("Verify accumulator value")
+    {
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(state.A == 0x8F);
+    }
+
+    SECTION("Verify N flag set")
+    {
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+
+    SECTION("Verify N flag cleared")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x1, 0xA2, 0x8F, 0x98});
+        emu.execute(6);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("Verify Z flag set")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x0, 0x98});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        auto state = cpu.get_CPU_state();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+    }
+
+    SECTION("Verify Z flag cleared")
+    {
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x1, 0xA2, 0x0, 0x98});
         emu.execute(6);
         auto cpu = emu.get_CPU_obj();
         auto state = cpu.get_CPU_state();
