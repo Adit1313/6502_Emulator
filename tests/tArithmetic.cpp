7b07fc584e473b4c4e415a1f5ba0adf1b7832bf6
Adit <aditkirtani@gmail.com>
2026-04-12 19:04:13 +0200

    Added CPX and CPY tests

--- tests/tArithmetic.cpp
+++ tests/tArithmetic.cpp
@@ -571,3 +571,466 @@ TEST_CASE("SBC with IZY addressing", "[SBC][IZY]")
 }
 
 #pragma endregion
+
+#pragma region CMP Tests
+
+TEST_CASE("CMP flag behaviour", "[CMP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("A > M (C=1, Z=0, N=0)")
+    {
+        // LDA #0x08, CMP #0x03
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x08, 0xC9, 0x03});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("A == M (C=1, Z=1, N=0)")
+    {
+        // LDA #0x05, CMP #0x05
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x05, 0xC9, 0x05});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("A < M (C=0, Z=0, N=1)")
+    {
+        // LDA #0x03, CMP #0x05
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x03, 0xC9, 0x05});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+
+    SECTION("Negative result without borrow edge case")
+    {
+        // LDA #0x80, CMP #0x7F => result positive, N=0
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x80, 0xC9, 0x7F});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("Sign bit propagation")
+    {
+        // LDA #0x00, CMP #0x80 => result = 0x80, N=1
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x00, 0xC9, 0x80});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+}
+
+TEST_CASE("CMP with IMM addressing", "[CMP][IMM]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Immediate compare greater")
+    {
+        // LDA #0x0A, CMP #0x02
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x0A, 0xC9, 0x02});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+
+    SECTION("Immediate compare equal")
+    {
+        // LDA #0x0A, CMP #0x0A
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x0A, 0xC9, 0x0A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+    }
+
+    SECTION("Immediate compare less")
+    {
+        // LDA #0x02, CMP #0x0A
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x02, 0xC9, 0x0A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 0);
+    }
+}
+
+TEST_CASE("CMP with ZP addressing", "[CMP][ZP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Zero page compare")
+    {
+        // mem[0x10]=0x05, LDA #0x08, CMP $10
+        emu.load_bytes_at_address(0x10, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x08, 0xC5, 0x10});
+        emu.execute(5);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CMP with ZPX addressing", "[CMP][ZPX]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("ZPX compare")
+    {
+        // X=1, mem[0x11]=0x05, LDA #0x08, CMP $10,X
+        emu.load_bytes_at_address(0x11, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x01, 0xA9, 0x08, 0xD5, 0x10});
+        emu.execute(6);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CMP with ABS addressing", "[CMP][ABS]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("ABS compare")
+    {
+        // mem[0x300]=0x05, LDA #0x08, CMP $0300
+        emu.load_bytes_at_address(0x300, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA9, 0x08, 0xCD, 0x00, 0x03});
+        emu.execute(6);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CMP with ABSX addressing", "[CMP][ABSX]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("ABSX compare")
+    {
+        // X=1, mem[0x301]=0x05, LDA #0x08, CMP $0300,X
+        emu.load_bytes_at_address(0x301, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x01, 0xA9, 0x08, 0xDD, 0x00, 0x03});
+        emu.execute(7);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CMP with ABSY addressing", "[CMP][ABSY]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("ABSY compare")
+    {
+        // Y=1, mem[0x301]=0x05, LDA #0x08, CMP $0300,Y
+        emu.load_bytes_at_address(0x301, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x01, 0xA9, 0x08, 0xD9, 0x00, 0x03});
+        emu.execute(7);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CMP with IZX addressing", "[CMP][IZX]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("IZX compare")
+    {
+        // X=1, ptr -> 0x300, mem[0x300]=0x05
+        emu.load_bytes_at_address(0x11, std::vector<u8> {0x00});
+        emu.load_bytes_at_address(0x12, std::vector<u8> {0x03});
+        emu.load_bytes_at_address(0x300, std::vector<u8> {0x05});
+
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x01, 0xA9, 0x08, 0xC1, 0x10});
+        emu.execute(8);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CMP with IZY addressing", "[CMP][IZY]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("IZY compare")
+    {
+        // ptr -> 0x300, Y=1 => 0x301, mem=0x05
+        emu.load_bytes_at_address(0x10, std::vector<u8> {0x00});
+        emu.load_bytes_at_address(0x11, std::vector<u8> {0x03});
+        emu.load_bytes_at_address(0x301, std::vector<u8> {0x05});
+
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x01, 0xA9, 0x08, 0xD1, 0x10});
+        emu.execute(7);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+#pragma endregion
+
+#pragma region CPX Tests
+
+TEST_CASE("CPX flag behaviour", "[CPX]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("X > M (C=1, Z=0, N=0)")
+    {
+        // LDX #0x08, CPX #0x03
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x08, 0xE0, 0x03});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("X == M (C=1, Z=1, N=0)")
+    {
+        // LDX #0x05, CPX #0x05
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x05, 0xE0, 0x05});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("X < M (C=0, Z=0, N=1)")
+    {
+        // LDX #0x03, CPX #0x05
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x03, 0xE0, 0x05});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+}
+
+TEST_CASE("CPX with IMM addressing", "[CPX][IMM]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Immediate compare greater")
+    {
+        // LDX #0x0A, CPX #0x02
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x0A, 0xE0, 0x02});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+
+    SECTION("Immediate compare equal")
+    {
+        // LDX #0x0A, CPX #0x0A
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x0A, 0xE0, 0x0A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+    }
+
+    SECTION("Immediate compare less")
+    {
+        // LDX #0x02, CPX #0x0A
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x02, 0xE0, 0x0A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 0);
+    }
+}
+
+TEST_CASE("CPX with ZP addressing", "[CPX][ZP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Zero page compare")
+    {
+        // mem[0x10]=0x05, LDX #0x08, CPX $10
+        emu.load_bytes_at_address(0x10, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x08, 0xE4, 0x10});
+        emu.execute(5);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CPX with ABS addressing", "[CPX][ABS]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Absolute compare")
+    {
+        // mem[0x300]=0x05, LDX #0x08, CPX $0300
+        emu.load_bytes_at_address(0x300, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA2, 0x08, 0xEC, 0x00, 0x03});
+        emu.execute(6);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CPY flag behaviour", "[CPY]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Y > M (C=1, Z=0, N=0)")
+    {
+        // LDY #0x08, CPY #0x03
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x08, 0xC0, 0x03});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("Y == M (C=1, Z=1, N=0)")
+    {
+        // LDY #0x05, CPY #0x05
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x05, 0xC0, 0x05});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 0);
+    }
+
+    SECTION("Y < M (C=0, Z=0, N=1)")
+    {
+        // LDY #0x03, CPY #0x05
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x03, 0xC0, 0x05});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 0);
+        REQUIRE(cpu.get_flag(CPU_6502::N) == 1);
+    }
+}
+
+TEST_CASE("CPY with IMM addressing", "[CPY][IMM]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Immediate compare greater")
+    {
+        // LDY #0x0A, CPY #0x02
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x0A, 0xC0, 0x02});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+
+    SECTION("Immediate compare equal")
+    {
+        // LDY #0x0A, CPY #0x0A
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x0A, 0xC0, 0x0A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::Z) == 1);
+    }
+
+    SECTION("Immediate compare less")
+    {
+        // LDY #0x02, CPY #0x0A
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x02, 0xC0, 0x0A});
+        emu.execute(4);
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 0);
+    }
+}
+
+TEST_CASE("CPY with ZP addressing", "[CPY][ZP]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Zero page compare")
+    {
+        // mem[0x10]=0x05, LDY #0x08, CPY $10
+        emu.load_bytes_at_address(0x10, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x08, 0xC4, 0x10});
+        emu.execute(5);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+TEST_CASE("CPY with ABS addressing", "[CPY][ABS]")
+{
+    Emulator emu;
+    emu.load_bytes_at_address(0xFFFC, std::vector<u8> {0x0, 0x2});
+    emu.reset(emu.RST_CPU);
+
+    SECTION("Absolute compare")
+    {
+        // mem[0x300]=0x05, LDY #0x08, CPY $0300
+        emu.load_bytes_at_address(0x300, std::vector<u8> {0x05});
+        emu.load_bytes_at_address(0x200, std::vector<u8> {0xA0, 0x08, 0xCC, 0x00, 0x03});
+        emu.execute(6);
+
+        auto cpu = emu.get_CPU_obj();
+        REQUIRE(cpu.get_flag(CPU_6502::C) == 1);
+    }
+}
+
+#pragma endregion
